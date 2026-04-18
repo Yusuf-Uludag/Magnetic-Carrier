8d1022ac7438e7d46dbaa09ddfee46d33ccf7f6d
Yusuf-Uludag <uludag.yusuf@metu.edu.tr>
2026-04-18 16:24:37 +0300

    Preventing collider polygon from being offset from texture polygon

--- scripts/collision_polygon_2d.gd
+++ scripts/collision_polygon_2d.gd
@@ -5,7 +5,7 @@ extends CollisionPolygon2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	polygon = polygon_2d.polygon
-	position = polygon_2d.position
+	position = polygon_2d.position + polygon_2d.offset
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
