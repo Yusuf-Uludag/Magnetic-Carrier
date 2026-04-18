47c807f986132ab48655e6491efec0ebb72b659e
Yusuf-Uludag <uludag.yusuf@metu.edu.tr>
2026-04-18 15:21:01 +0300

    Add Platform Scene for walls floor and ceiling
    Add platform_polygon to your scene, right-click and open editable child. Draw the vertices of the Polygon2D and you have a collidable polygon with a texture.

--- scripts/collision_polygon_2d.gd
+++ scripts/collision_polygon_2d.gd
@@ -1,8 +1,11 @@
-extends Node2D
+extends CollisionPolygon2D
+
+@onready var polygon_2d: Polygon2D = $"../Polygon2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	polygon = polygon_2d.polygon
+	position = polygon_2d.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
