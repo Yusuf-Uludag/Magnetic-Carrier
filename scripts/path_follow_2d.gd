e660ce4bd2579279fcca3094ccef3c6828425e60
Yusuf-Uludag <uludag.yusuf@metu.edu.tr>
2026-04-21 15:16:56 +0300

    Commented the code structure of hatches
    Buttons are required for script to work

--- scripts/path_follow_2d.gd
+++ scripts/path_follow_2d.gd
@@ -1,7 +1,8 @@
 extends PathFollow2D
 
-# func _on_button_pressed():
-#     
+#	button press signal function
+#	func _on_button_pressed():
+#		progress_ratio = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +11,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# signal from button triggering progress 0.0 -> 1.0
-	# _on_button_pressed()
-	pass
+#	 signal = button triggering progress 0.0 -> 1.0
+#		if signal:
+#			_on_button_pressed()
+	progress_ratio += 0.015
