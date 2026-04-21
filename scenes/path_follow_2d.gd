3fdd3b03f9972c245b93e7b6fca6bab2099c1101
Yusuf-Uludag <uludag.yusuf@metu.edu.tr>
2026-04-21 16:21:02 +0300

    Add buttons along with their test scene

---
+++ scenes/path_follow_2d.gd
@@ -0,0 +1,13 @@
+extends PathFollow2D
+
+var time = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time += delta * 1.2
+	progress_ratio = (sin(time) + 1)/2
