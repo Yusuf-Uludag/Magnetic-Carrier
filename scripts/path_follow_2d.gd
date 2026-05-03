d1414499af9530353b79628d53b8e21192290369
Muhammed Enes Karaca <eneskaracap@gmail.com>
2026-05-03 22:42:07 +0300

    scripts moved from scenes folder and add platform test scene

--- scripts/path_follow_2d.gd
+++ scripts/path_follow_2d.gd
@@ -1,17 +1,13 @@
 extends PathFollow2D
 
-#	button press signal function
-#	func _on_button_pressed():
-#		progress_ratio = 1
+var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-#	 signal = button triggering progress 0.0 -> 1.0
-#		if signal:
-#			_on_button_pressed()
-	progress_ratio += 0.015
+	time += delta * 1.2
+	progress_ratio = (sin(time) + 1)/2
